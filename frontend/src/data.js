9b8f97e56ad1066965683ddfe700f19697b24c9b
Juan Pablo Quintero <quintejps@gmail.com>
2021-02-19 12:39:05 -0700

    share code on github

---
+++ frontend/src/data.js
@@ -0,0 +1,65 @@
+const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'BSN Syntha-6 Isolate',
+            category: 'Gainer',
+            image: '/images/p1.jpg',
+            price: '$120',
+            brand: 'BSN',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'suplemento de proteínas en polvo más novedoso de BSN'
+
+        },
+        {
+            _id: '2',
+            name: 'Muscletech Mass-Tech',
+            category: 'Gainer',
+            image: '/images/p2.jpg',
+            price: '$860',
+            brand: 'Muscletech',
+            rating: 4.5,
+            numReviews: 12,
+            description: 'suplemento de proteínas en polvo más novedoso de Muscletech'
+
+        },
+        {
+            _id: '3',
+            name: 'Dymatize Super Mass Gainer',
+            category: 'Gainer',
+            image: '/images/p3.jpg',
+            price: '$600',
+            brand: 'Dymatize',
+            rating: 3.5,
+            numReviews: 7,
+            description: 'suplemento de proteínas en polvo más novedoso de Dymatize'
+
+        },
+        {
+            _id: '4',
+            name: 'Optimum Nutrition Serious Mass Gainer',
+            category: 'Gainer',
+            image: '/images/p4.jpg',
+            price: '$1000',
+            brand: 'Optimum Nutrition',
+            rating: 5.0,
+            numReviews: 14,
+            description: 'suplemento de proteínas en polvo más novedoso de Optimum Nutrition'
+
+        },
+        {
+            _id: '5',
+            name: 'Myogenix Aftershock',
+            category: 'Gainer',
+            image: '/images/p5.jpg',
+            price: '$800',
+            brand: 'Myogenix',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'suplemento de proteínas en polvo más novedoso de Myogenix'
+
+        },
+    ],
+};
+export default data;
