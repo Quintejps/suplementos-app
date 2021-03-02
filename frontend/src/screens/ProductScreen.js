f46feb059ca99a09c48be7df9aa9b065b111c821
Juan Pablo Quintero <quintejps@gmail.com>
2021-03-01 18:15:55 -0700

    Fetching data from backend

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -45,8 +45,10 @@ export default function ProductScreen(props) {
                                 <div className="row">
                                     <div>Status</div>
                                     <div>
-                                        {product.countInStock > 0 ? <span className="success">In Stock</span> :
-                                            <span className="error">Unavailable</span>
+                                        {product.countInStock > 0 ?
+                                            <span className="success">In Stock</span>
+                                            :
+                                            <span className="danger">Unavailable</span>
                                         }
                                     </div>
                                 </div>
